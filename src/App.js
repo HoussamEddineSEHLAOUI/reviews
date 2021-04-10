0f1d15bbb362147e51342b9323e5bf7706fd1a5b
HoussamEddineSEHLAOUI <sehlaoui.houssameddine@gmail.com>
2021-04-10 18:52:57 +0200

    Add all my files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+import './App.css';
+import  ReviewComp from './reviewComp';
+import users from './Data';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      i:0
+    }
+  }
+
+
+  onClickLeft=()=>{
+    console.log("click left");
+    const iOld=this.state.i;
+    if(iOld === 0){
+      this.setState({i:users.length-1});
+    }else{
+      this.setState({i:iOld-1});
+    }
+  }
+
+  onClickRight=()=>{
+    console.log("click right")
+    const iOld=this.state.i;
+    if(iOld === users.length-1){
+      this.setState({i:0});
+    }else{
+      this.setState({i:iOld+1});
+    }
+  }
+
+  onClickRanddom=()=>{
+    console.log("click random")
+    this.setState({i:1});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+        <ReviewComp 
+          onClickLeft={this.onClickLeft}
+          onClickRight={this.onClickRight}
+          onClickRanddom={this.onClickRanddom}
+          user={users[this.state.i]}
+          />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
